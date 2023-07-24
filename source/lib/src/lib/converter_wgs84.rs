60e742a72ff6de2631c65c94d5e4db711b4e2cd5
Artur Smirnov <artur.smirnov@innovatrieste.it>
2023-07-24 16:31:55 +0200

    wip

---
+++ source/lib/src/lib/converter_wgs84.rs
@@ -0,0 +1,123 @@
+
+pub struct WGS84 {
+    pub wgs84: String,
+    pub shape: String,
+    pub latitude: f32,
+    pub longitude: f32,
+    pub inner_radius: u16,
+    pub uncertainty_radius: u16,
+    pub offset_angle: u16,
+    pub included_angle: u16,
+    pub confidence: u8,
+}
+
+impl WGS84 {
+    pub fn encode(&mut self) {
+        self.wgs84 = (&"A0").to_string();
+        self.wgs84 += &Self::enc_latitude(self.latitude);
+        self.wgs84 += &Self::enc_longitude(self.longitude);
+        self.wgs84 += &Self::enc_inner_radius(self.inner_radius);
+        self.wgs84 += &Self::enc_uncertainty_radius(self.uncertainty_radius);
+        self.wgs84 += &Self::enc_offset_angle(self.offset_angle);
+        self.wgs84 += &Self::enc_included_angle(self.included_angle);
+        self.wgs84 += &Self::enc_confidence(self.confidence);
+    }
+
+    pub fn decode(&self) {}
+
+    fn enc_latitude(latitude: f32) -> String {
+        let is_neg: bool = latitude < 0.0;
+        let lat: f32 = (2_f32.powf(23.0) * latitude.abs()) / 90.0;
+        let lat: u32 = lat.round() as u32;
+
+        let enc: u32 = (is_neg as u32) << 23 as u32 | lat;
+
+        return format!("{:0>6X}", enc);
+    }
+
+    fn enc_longitude(longitude: f32) -> String {
+        let is_neg: bool = longitude < 0.0;
+        let long: f32 = (2_f32.powf(24.0) * longitude.abs()) / 360.0;
+        let long: u32 = long.round() as u32;
+
+        let mut _enc = format!("{:0>24b}", long);
+
+        if is_neg {
+            _enc = Self::to_twos_complement(_enc)
+        }
+
+        let enc = isize::from_str_radix(&_enc, 2).unwrap();
+
+        return format!("{:0>6X}", enc);
+    }
+
+    fn enc_inner_radius(ir: u16) -> String {
+        let ir: u16 = ir / 5;
+        let enc: String = format!("{:0>4X}", ir);
+
+        return enc;
+    }
+
+    fn enc_uncertainty_radius(ur: u16) -> String {
+        let ur: f32 = ((10 + ur) / 10) as f32;
+
+        let _enc: f32 = ur.log(1.1);
+        let _enc: u16 = _enc.round() as u16;
+
+        let enc: String = format!("{:0>2X}", _enc);
+
+        return enc;
+    }
+
+    fn enc_offset_angle(oa: u16) -> String {
+        let oa: u16 = oa / 2;
+        let enc: String = format!("{:0>2X}", oa);
+
+        return enc;
+    }
+
+    fn enc_included_angle(ia: u16) -> String {
+        let ia: u16 = ia / 2;
+        let enc: String = format!("{:0>2X}", ia);
+
+        return enc;
+    }
+
+    fn enc_confidence(cf: u8) -> String {
+        let enc: String = format!("{:0>2X}", cf);
+
+        return enc;
+    }
+
+    // =====================================================================
+
+    fn to_twos_complement(s: String) -> String {
+        let mut num: String = s.to_string();
+        let n = num.len();
+
+        let mut i = n as isize - 1;
+        while let Some(c) = num.chars().nth(i as usize) {
+            if c == '1' {
+                break;
+            }
+            i -= 1;
+        }
+
+        if i == -1 {
+            num = format!("1{}", num);
+        } else {
+            let mut k = i - 1;
+            while k >= 0 {
+                let c = num.chars().nth(k as usize).unwrap();
+                if c == '1' {
+                    num.replace_range(k as usize..=k as usize, "0");
+                } else {
+                    num.replace_range(k as usize..=k as usize, "1");
+                }
+                k -= 1;
+            }
+        }
+
+        return num;
+    }
+}
--- source/lib/target/debug/deps/cell_tower_geolocation-6b3eaa947c356c73.d
+++ source/lib/target/debug/deps/cell_tower_geolocation-6b3eaa947c356c73.d
@@ -1,5 +1,5 @@
-/workspaces/cell_tower_geolocation/target/debug/deps/cell_tower_geolocation-6b3eaa947c356c73: src/main.rs
+/workspaces/cell_tower_geolocation/source/lib/target/debug/deps/cell_tower_geolocation-6b3eaa947c356c73: src/main.rs
 
-/workspaces/cell_tower_geolocation/target/debug/deps/cell_tower_geolocation-6b3eaa947c356c73.d: src/main.rs
+/workspaces/cell_tower_geolocation/source/lib/target/debug/deps/cell_tower_geolocation-6b3eaa947c356c73.d: src/main.rs
 
 src/main.rs:
