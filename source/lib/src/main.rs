2414b4c00c33bd1e4e4e8613efc363c1668baebd
Artur Smirnov <artur.smirnov@innovatrieste.it>
2023-07-25 10:34:36 +0200

    done

--- source/lib/src/main.rs
+++ source/lib/src/main.rs
@@ -1,18 +1,18 @@
 use cell_tower_geolocation::WGS84;
 
 fn main() {
-    let mut coordinates = WGS84 {
+    let mut wgs84 = WGS84 {
         wgs84: String::new(),
         shape: "A0".to_string(),
         latitude: 51.508037,
         longitude: 12.751280,
-        inner_radius: 55,
-        uncertainty_radius: 57,
-        offset_angle: 348,
-        included_angle: 66,
+        inner_radius: 8500,
+        uncertainty_radius: 400,
+        offset_angle: 280,
+        included_angle: 30,
         confidence: 90,
     };
 
-    coordinates.encode();
-    coordinates.display();
+    wgs84.encode();
+    wgs84.display();
 }
